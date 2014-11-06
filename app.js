dbcc6eac165e865b1fe08761b60f1f732a69dbf8
pratik <pratik60@gmail.com>
2014-11-06 06:55:17 +0200

    Return io as a part of MeanSocket object.

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var MeanSocket = new Module('socket');
 MeanSocket.register(function(app, auth, database, http) {
 
     var io = require('./server/config/socketio')(http);
+    MeanSocket.io = io;
 
     //We enable routing. By default the Package Object is passed to the routes
     MeanSocket.routes(io);
